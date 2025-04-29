37ed0b5fbadbc86ab9c63ae9ec8490065f4bfcd4
Caleb Blevins <blevsca2@gmail.com>
2025-04-29 10:25:45 -0400

    initial project format

---
+++ sub_strings.rb
@@ -0,0 +1,19 @@
+dictionary = [
+    "hello"
+    "ruby"
+    "program"
+    "developer"
+    "submarine"
+    "alphabet"
+    "banana"  
+    "wizard"
+    "castle"
+    "thunder"
+    "sandwich"
+    "butterfly"
+    "universe"
+]
+
+def substrings
+   
+end
